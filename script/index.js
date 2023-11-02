dcff8eebd9a88501edc0db77567b5e5e1ae39fc1
Yelyzaveta Berest <lizaberest97@gmail.com>
2023-11-02 20:26:50 +0200

    create custom select, set error from server

--- script/index.js
+++ script/index.js
@@ -4,14 +4,6 @@ const CLASS_FORM_IN = 'form__in'
 const CLASS_FORM_OUT = 'form__out'
 const CLASS_FORM_ACTIVE = 'form__active'
 
-const ERRORS = {
-    "gender": "Please select a gender",
-    "age": "Please select an age",
-    "location": "Please enter a valid location",
-    "email": "Please enter a valid email address",
-    "password": "Please enter a password"
-}
-
 const REGEX_VALID = {
     "location": /^[#.0-9a-zA-Z\s,-]{8,20}$/,
     "email": /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
@@ -26,54 +18,79 @@ const nextBtn = document.querySelector('.form__nav-btn_next')
 const progress = document.querySelectorAll('.form__progress span');
 const startBtn = document.querySelector('.form__nav-btn_start')
 const errorMessage = document.querySelector('.form__item .error-message')
-
-
-
-
-
-nextBtn.addEventListener('click', onNextBtnClick)
-returnBtn.addEventListener('click', onReturnBtnClick)
-startBtn.addEventListener('click', onStartBtnClick)
-
+const formGender = document.querySelector('#gender')
+const formAge = document.querySelector('#age')
 
 let active = 0
 slideForm()
 
+nextBtn.addEventListener('click', onNextBtnClick)
+returnBtn.addEventListener('click', onReturnBtnClick)
+formGender.addEventListener('click', onFormGenderClick)
+formAge.addEventListener('click', onDFormAgeClick)
+startBtn.addEventListener('click', onStartBtnClick)
+
+function onFormGenderClick(e) {
+    formGender.classList.toggle('is-active')
+    onOptionClick(formGender)
+
+}
+
+function onOptionClick(item) {
+    item.querySelectorAll('input').forEach(el => {
+        el.addEventListener('click', (e) => {
+            if (el.checked) {
+                e.stopPropagation()
+                setTimeout(() => {
+                    item.classList.remove('is-active')
+                })
+                item.querySelector('.select-dropdown__select').innerHTML = el.value
+            }
+        })
+    })
+}
+
+function onDFormAgeClick(e) {
+    formAge.classList.toggle('is-active')
+    onOptionClick(formAge)
+
+}
+
+
+
 
 function onNextBtnClick() {
-    
     const formSelect = isFormSelect()
+
     if (formSelect) {
-        const value = formSelect.options[formSelect.selectedIndex].value
+        const value = getSelectValue(formSelect)
         if (!value) {
-            setError(formSelect)
+            setError(formSelect.id)
             return
         }
         removeError()
-        console.log(value);
         active = active + 1 < formItems.length ? active + 1 : active;
         slideForm();
         return
     }
     const input = formItems[active].querySelector('input')
     if (!REGEX_VALID[input.name].test(input.value)) {
-        setError(input)
+        setError(input.name)
         return
     }
     removeError()
     if (active === formItems.length - 2) {
         nextBtn.classList.add(CLASS_DISPLAY_NONE)
         startBtn.classList.remove(CLASS_DISPLAY_NONE)
-
     }
-    console.log(input.value);
+
     active = active + 1 < formItems.length ? active + 1 : active;
     slideForm();
 
 }
 
 function onReturnBtnClick() {
-    if(nextBtn.classList.contains(CLASS_DISPLAY_NONE)) {
+    if (nextBtn.classList.contains(CLASS_DISPLAY_NONE)) {
         nextBtn.classList.remove(CLASS_DISPLAY_NONE)
         startBtn.classList.add(CLASS_DISPLAY_NONE)
     }
@@ -84,35 +101,59 @@ function onReturnBtnClick() {
 function onStartBtnClick() {
     const input = formItems[active].querySelector('input')
     if (!REGEX_VALID[input.name].test(input.value)) {
-        setError(input)
+        setError(input.name)
         return
     }
-    console.log(input.value);
     removeError()
 }
 
-function postDataToServer() {
-
+function isFormSelect() {
+    return formItems[active].querySelector('.select-dropdown__form')
 }
 
-function isFormSelect() {
-    return formItems[active].querySelector('select')
+function getSelectValue(formSelect) {
+    let value
+    formSelect.querySelectorAll('input').forEach((option) => {
+        if (option.checked) {
+            value = option.value
+        }
+    })
+    return value
+}
+
+async function setError(name) {
+    if (formItems[active].querySelector(SELECTOR_ERROR_MESSAGE)) {
+        return
+    }
+    console.log(name);
+    const errorTemplate = await getErrorTemplate(name)
+    formItems[active].insertAdjacentHTML('beforeend', errorTemplate)
+}
+
+async function getErrorTemplate(name) {
+    const errorList = await fetchError()
+    console.log(name);
+    const errorMessage = errorList[name][0]
+    console.log(errorMessage);
+    return `<p class="error-message">${errorMessage}</p>`
+}
+
+async function fetchError() {
+    const { errors } = await fetch('https://run.mocky.io/v3/f6ca495a-0a08-40de-9889-e73d49d011d2')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Failed to send data: ${response.status}`);
+            }
+            return response.json();
+        })
+        .catch((e) => console.error(e))
+    return errors
 }
 
 function removeError() {
     if (formItems[active].querySelector(SELECTOR_ERROR_MESSAGE)) {
         formItems[active].querySelector(SELECTOR_ERROR_MESSAGE).remove()
     }
-} 
-
-function setError(element) {
-    const errorTemplate = getErrorTemplate(element.name)
-    formItems[active].insertAdjacentHTML('beforeend', errorTemplate)
-
-}
-
-function getErrorTemplate(name) {
-    return `<p class="error-message">${ERRORS[name]}</p>`
 }
 
 function slideForm() {
@@ -123,8 +164,7 @@ function slideForm() {
     for (let i = active + 1; i < formItems.length; i++) {
         formItems[i].classList.remove(CLASS_FORM_OUT)
         formItems[i].classList.add(CLASS_FORM_IN)
-        progress[i].style.background = ""
-
+        progress[i].style.backgroundColor = "transparent"
     }
 
     for (let i = active - 1; i >= 0; i--) {
